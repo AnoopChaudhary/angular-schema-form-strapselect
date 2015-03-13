5a4aa164b7ae425a4e2e68cd83ec474bbf44f218
Steve Hu <stevehu@gmail.com>
2015-03-12 23:08:45 -0400

    merge code from Nicklas

--- app.js
+++ app.js
@@ -14,7 +14,29 @@ var lightApp = angular.module('lightApp', ['mgcrea.ngStrap', 'schemaForm', 'pasc
     lightApp.factory    = $provide.factory;
     lightApp.service    = $provide.service;
 }])
-.controller('SelectController', function($scope){
+.controller('SelectController',[ '$scope','$http', function($scope, $http){
+
+  $scope.callBackSD = function (options) {
+      return [
+          {value: 'value1', label: 'label1'},
+          {value: 'value2', label: 'label2'},
+          {value: 'value3', label: 'Dynamic select!'}
+        ]
+  };
+
+  $scope.callBackMSD = function (options) {
+      return [
+          {value: 'value1', label: 'label1'},
+          {value: 'value2', label: 'label2'},
+          {value: 'value3', label: 'Multiple dynamic select!'}
+        ]
+  };
+
+
+  $scope.callBackMSDAsync = function (options) {
+      return $http.get(options.async.url);
+  };
+
   $scope.schema = {
     type: 'object',
     title: 'Select',
@@ -22,7 +44,6 @@ var lightApp = angular.module('lightApp', ['mgcrea.ngStrap', 'schemaForm', 'pasc
       select: {
         title: 'Single Select Static',
         type: 'string',
-        format: 'strapselect',
         description: 'Only single item is allowed',
         items: [
           {value: 'value1', label: 'label1'},
@@ -33,50 +54,95 @@ var lightApp = angular.module('lightApp', ['mgcrea.ngStrap', 'schemaForm', 'pasc
       multiselect: {
         title: 'Multi Select Static',
         type: 'array',
-        format: 'strapselect',
-        description: 'Multi single items are allowed',
+        description: 'Multi single items are allowed(select third for error)',
         items: [
-          {value: 'value1', label: 'label1'},
-          {value: 'value2', label: 'label2'},
+          {value: ['value1', 'value1a'], label: 'label1'},
+          {value: ['value2', 'value2'], label: 'label2'},
           {value: 'value3', label: 'long very very long label3'}
         ]
       },
         selectdynamic: {
             title: 'Single Select Dynamic',
             type: 'string',
-            format: 'strapselectdynamic',
-            description: 'Must be running on Light Server which is listening to port 8080'
+            description: 'This data is loaded from the $scope.callBackSD function.'
         },
         multiselectdynamic: {
             title: 'Multi Select Dynamic',
             type: 'array',
-            format: 'strapselectdynamic',
-            description: 'Must be running on Light Server which is listening to port 8080'
+            description: 'This data is loaded from the $scope.callBackMSD function.'
+        },
+        multiselectdynamic_http_post: {
+            title: 'Multi Select Dynamic HTTP Post',
+            type: 'array',
+            description: 'This data is asynchrously loaded using a HTTP post(specify options.url and options.parameter)'
+        },
+        multiselectdynamic_http_get: {
+            title: 'Multi Select Dynamic HTTP Post',
+            type: 'array',
+            description: 'This data is asynchrously loaded using a HTTP get(specify options.url)'
+        },
+        multiselectdynamic_async: {
+            title: 'Multi Select Dynamic Async',
+            type: 'array',
+            description: 'This data is asynchrously loaded using a async call(specify options.async.call)'
         }
     },
     required: ['select', 'multiselect']
   };
   $scope.form = [
      {
-       key: 'select'
+       key: 'select',
+       type: 'strapselect'
+
      },
      {
-       key: 'multiselect'
+       key: 'multiselect',
+       type: 'strapselect'
      },
      {
-       "key": "selectdynamic",
-       "options": {
-         "category": "forum",
-         "name": "getForumDropdown"
+       key: "selectdynamic",
+       type: 'strapselectdynamic',
+       options: {
+         callback: $scope.callBackSD
        }
      },
      {
-       "key": "multiselectdynamic",
-         "options": {
-         "category": "forum",
-         "name": "getForumDropdown"
+       key: "multiselectdynamic",
+       type: 'strapmultiselectdynamic',
+       options: {
+           callback: $scope.callBackMSD
        }
      },
+     {
+       key: "multiselectdynamic_http_post",
+       type: 'strapmultiselectdynamic',
+       options: {
+           http_post: {
+               url : "http://example:8080/api/rs",
+               parameter: { category: "demo",  name: "getDropdown", readOnly: true}
+           }
+       }
+     },
+     {
+       key: "multiselectdynamic_http_get",
+       type: 'strapmultiselectdynamic',
+       options: {
+           http_get: {
+               url : "http://example:8080/api/rs?cmd={\"category\": \"demo\", \"name\": \"getDropdown\", \"readOnly\": true}"
+           }
+       }
+     },
+     {
+       key: "multiselectdynamic_async",
+       type: 'strapmultiselectdynamic',
+       options: {
+           async: {
+               call: $scope.callBackMSDAsync,
+               url : "/test/testdata.json"
+           }
+       }
+     },
+
      {
        type: "submit",
        style: "btn-info",
@@ -88,8 +154,11 @@ var lightApp = angular.module('lightApp', ['mgcrea.ngStrap', 'schemaForm', 'pasc
   $scope.model.select = 'value3';
   $scope.model.multiselect = ['value2', 'value1'];
 
+
   $scope.submitted = function(form){
     $scope.$broadcast('schemaFormValidate')
     console.log($scope.model);
   };
-});
+}]);
+
+
--- src/schema-form-strapselect.js
+++ src/schema-form-strapselect.js
@@ -3,7 +3,7 @@ angular.module('schemaForm').config(
   function(schemaFormProvider,  schemaFormDecoratorsProvider, sfPathProvider) {
 
     var select = function(name, schema, options) {
-      if (schema.type === 'string' && schema.format == 'strapselect') {
+      if (schema.type === 'string') {
         var f = schemaFormProvider.stdFormObj(name, schema, options);
         f.key  = options.path;
         f.type = 'strapselect';
@@ -15,7 +15,7 @@ angular.module('schemaForm').config(
     schemaFormProvider.defaults.string.unshift(select);
 
     var multiselect = function(name, schema, options) {
-      if (schema.type === 'array' && schema.format == 'strapselect') {
+      if (schema.type === 'array') {
         var f = schemaFormProvider.stdFormObj(name, schema, options);
         f.key  = options.path;
         f.type = 'strapmultiselect';
@@ -27,7 +27,7 @@ angular.module('schemaForm').config(
     schemaFormProvider.defaults.array.unshift(multiselect);
 
     var selectdynamic = function(name, schema, options) {
-      if (schema.type === 'string' && schema.format == 'strapselectdynamic') {
+      if (schema.type === 'string') {
         var f = schemaFormProvider.stdFormObj(name, schema, options);
         f.key  = options.path;
         f.type = 'strapselectdynamic';
@@ -39,7 +39,7 @@ angular.module('schemaForm').config(
     schemaFormProvider.defaults.string.unshift(selectdynamic);
 
     var multiselectdynamic = function(name, schema, options) {
-      if (schema.type === 'array' && schema.format == 'strapselectdynamic') {
+      if (schema.type === 'array') {
         var f = schemaFormProvider.stdFormObj(name, schema, options);
         f.key  = options.path;
         f.type = 'strapmultiselectdynamic';
@@ -55,6 +55,7 @@ angular.module('schemaForm').config(
     'directives/decorators/bootstrap/strap/strapselect.html');
     schemaFormDecoratorsProvider.createDirective('strapselect',
     'directives/decorators/bootstrap/strap/strapselect.html');
+
     schemaFormDecoratorsProvider.addMapping('bootstrapDecorator', 'strapmultiselect',
     'directives/decorators/bootstrap/strap/strapmultiselect.html');
     schemaFormDecoratorsProvider.createDirective('strapmultiselect',
@@ -64,9 +65,59 @@ angular.module('schemaForm').config(
         'directives/decorators/bootstrap/strap/strapselectdynamic.html');
     schemaFormDecoratorsProvider.createDirective('strapselectdynamic',
         'directives/decorators/bootstrap/strap/strapselectdynamic.html');
+
     schemaFormDecoratorsProvider.addMapping('bootstrapDecorator', 'strapmultiselectdynamic',
         'directives/decorators/bootstrap/strap/strapmultiselectdynamic.html');
     schemaFormDecoratorsProvider.createDirective('strapmultiselectdynamic',
         'directives/decorators/bootstrap/strap/strapmultiselectdynamic.html');
 
   }]);
+
+angular.module('schemaForm').controller('StrapSelectController', ['$scope', '$http', function ($scope, $http) {
+
+
+    $scope.items = [];
+
+    $scope.fetchResult = function (options) {
+        if (options.callback) {
+            $scope.items = options.callback(options);
+            console.log('items', $scope.items);
+        }
+        else if (options.http_post) {
+            return $http.post(options.http_post.url, options.http_post.parameter).then(
+                function (_data) {
+                    $scope.items = _data.data;
+                    console.log('items', $scope.items);
+                },
+                function (data, status) {
+                    alert("Loading select items failed (URL: '" + String(options.http_post.url) +
+                        "' Parameter: " + String(options.http_post.parameter) + "\nError: "  + status);
+                });
+        }
+        else if (options.http_get) {
+            return $http.get(options.http_get.url, options.http_get.parameter).then(
+                function (_data) {
+                    $scope.items = _data.data;
+                    console.log('items', $scope.items);
+                },
+                function (data, status) {
+                    alert("Loading select items failed (URL: '" + String(options.http_get.url) +
+                        "\nError: "  + status);
+                });
+        }
+        else if (options.async) {
+            return options.async.call(options).then(
+                function (_data) {
+                    $scope.items = _data.data;
+                    console.log('items', $scope.items);
+                },
+                function (data, status) {
+                    alert("Loading select items failed(Options: '" + String(options) +
+                        "\nError: "  + status);
+                });
+        }
+    };
+
+}]);
+
+
